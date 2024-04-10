e0dc8306b11f47e1ae8b70e1da954e0fbe952ddb
israeludofia1990 <udofiaisrael170@gmail.com>
2024-04-10 18:14:44 +0100

    multiple commits

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,35 +1,23 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
+    "env": {
+        "browser": true,
+        "es6": true
     },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+    "extends": [
+        "airbnb-base"
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
